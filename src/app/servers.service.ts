43052e6be5e3727ee43c735b4ddea17d36c64112
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-24 15:00:43 -0500

    put request to fb

--- src/app/servers.service.ts
+++ src/app/servers.service.ts
@@ -10,7 +10,8 @@ export class ServerService {
     storeServers(servers: any[]) {
 // tslint:disable-next-line: deprecation
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('https://angularhttp-900e6.firebaseio.com/data.json', servers, {headers});
+        // return this.http.post('https://angularhttp-900e6.firebaseio.com/data.json', servers, {headers});
+        return this.http.put('https://angularhttp-900e6.firebaseio.com/data.json', servers, {headers})
     }
 
     getServers() {
